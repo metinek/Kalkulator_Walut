5b6b84557cda29e48e403102c0622c6d8bb9449b
metinek <ut_3612@zsp3zamosc.pl>
2023-10-11 09:56:33 +0200

    commit

--- Kalkulator_Walut/MainPage.xaml.cs
+++ Kalkulator_Walut/MainPage.xaml.cs
@@ -1,12 +1,58 @@
-﻿namespace Kalkulator_Walut
+﻿using System.Net;
+using System.Collections.Specialized;
+using System.Text;
+using System.Text.Json;
+
+
+
+namespace Kalkulator_Walut
 {
+
+
+
+
     public partial class MainPage : ContentPage
     {
+        private string kodWaluty = "eur";
+        private string data = "";
+        public double skup = 0;
+        public double sprzedaz = 0;
+
+
+
         bool wybor = true;
         public MainPage()
         {
             InitializeComponent();
+            pobierzDane();
         }
+
+
+        private void pobierzDane()
+        {
+            string url = "https://api.nbp.pl/api/exchangerates/rates/c/" + kodWaluty + "/2023-10-11/?format=json";
+            string wynik;
+            using (var webClient = new WebClient())
+            {
+                wynik = webClient.DownloadString(url);
+            }
+            using JsonDocument j1 = JsonDocument.Parse(wynik);
+            JsonElement json = j1.RootElement;
+            var rates = json.GetProperty("rates");
+            var rate = rates[0];
+            string bid = rate.GetProperty("bid").ToString();
+            bid = bid.Replace('.', ',');
+
+            string ask = rate.GetProperty("ask").ToString();
+            ask = ask.Replace('.', ',');
+
+            data = rate.GetProperty("effectiveDate").ToString();
+            skup = double.Parse(bid);
+            sprzedaz = double.Parse(ask);
+            kursLbl.Text = "\nSkup: "+ Math.Round(skup, 2).ToString()+"\n";
+            kursLbl.Text += "Sprzedaż: "+ Math.Round(sprzedaz, 2).ToString();
+        }
+
         private void OnEuroClicked(object sender, EventArgs e)
         {
             euroNaPlnBtn.IsEnabled = false;
@@ -23,12 +69,17 @@
 
         private void OnPrzeliczClicked(object sender, EventArgs e)
         {
-            if (wybor==false)
-                otrzymaszLbl.Text = ( Math.Round(float.Parse(kwotaEnt.Text) * float.Parse(kursLbl.Text),2)).ToString() + "PLN";
+            if (wybor == false)
+            {
+                otrzymaszLbl.Text = (Math.Round(double.Parse(kwotaEnt.Text) * sprzedaz, 2)).ToString();
+                walutaLbl.Text = "PLN";
+            }
             else
-                otrzymaszLbl.Text = (Math.Round(float.Parse(kwotaEnt.Text) / float.Parse(kursLbl.Text),2)).ToString() + "€";
+            {
+                otrzymaszLbl.Text = (Math.Round(double.Parse(kwotaEnt.Text) / skup, 2)).ToString();
+                walutaLbl.Text = "€";
+            }
             SemanticScreenReader.Announce(otrzymaszLbl.Text);
-            lblJSON.Text = "";
         }
     }
 }
